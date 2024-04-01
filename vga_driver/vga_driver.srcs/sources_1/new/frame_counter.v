1bde9dcde12bdd196cc3b4bd2cc910025c8a7f03
Chris <chrisokoro21@gmail.com>
2024-04-01 00:06:12 -0700

    added my vga controller project

---
+++ vga_driver/vga_driver.srcs/sources_1/new/frame_counter.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/31/2024 11:30:57 PM
+// Design Name: 
+// Module Name: frame_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module frame_counter(
+
+    );
+endmodule
